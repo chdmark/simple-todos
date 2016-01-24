8638c147f0ce20709b631bf087b6cd5d9d1e23b4
Chris Mark <chdmark@gmail.com>
2016-01-23 17:20:05 -0800

    Add incomplete count in header.:

--- simple-todos.js
+++ simple-todos.js
@@ -4,7 +4,22 @@ if (Meteor.isClient) {
   
   Template.body.helpers({
    tasks: function () {
-    return Tasks.find({}, {sort: {createdAt: -1}});
+    if (Session.get("hideCompleted")) {
+      return Tasks.find({checked: {$ne: true}}, {sort: {createdAt: -1}});
+    } else {
+
+      return Tasks.find({}, {sort: {createdAt: -1}});
+    }
+   },
+
+   hideCompleted: function(){
+    return Session.get("hideCompleted");
+
+   },
+
+   incompleteCount: function () {
+    return Tasks.find({checked: {$ne: true}}).count();
+
    }
   });
 
@@ -24,6 +39,10 @@ if (Meteor.isClient) {
  
       // Clear form
       event.target.text.value = "";
+    },
+
+    "change .hide-completed input": function (event) {
+      Session.set("hideCompleted", event.target.checked);
     }
   });
 
